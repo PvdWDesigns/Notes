67725dd4fe1516aca985d6290f4255bb80659c56
Petrie <PvdWDesigns@users.noreply.github.com>
2020-07-16 08:39:29 +0200

    Added stuff

--- Notes/Notes/App.xaml.cs
+++ Notes/Notes/App.xaml.cs
@@ -1,28 +1,19 @@
 ﻿using System;
+using System.IO;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace Notes
 {
     public partial class App : Application
     {
+        public static string FolderPath { get; private set; }
+
         public App()
         {
             InitializeComponent();
-
-            MainPage = new MainPage();
+            FolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
+            MainPage = new NavigationPage(new NotesPage());
         }
-
-        protected override void OnStart()
-        {
-        }
-
-        protected override void OnSleep()
-        {
-        }
-
-        protected override void OnResume()
-        {
-        }
+        // ...
     }
 }
